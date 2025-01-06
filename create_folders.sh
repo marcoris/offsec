c1e84c42f660a159a235a13c6fcff1321eafd777
Marco Ris <marco.ris@risdesign.ch>
2025-01-06 17:16:40 +0100

    Create create_folders.sh

---
+++ create_folders.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Creating scripts folder
+mkdir -p /home/kali/Pentesting/scripts/bash
+mkdir -p /home/kali/Pentesting/scripts/php
+
+# Creating learning folder
+mkdir -p /home/kali/Learning/thm/windows/easy
+mkdir -p /home/kali/Learning/thm/windows/medium
+mkdir -p /home/kali/Learning/thm/windows/hard
+mkdir -p /home/kali/Learning/thm/windows/insane
+mkdir -p /home/kali/Learning/thm/linux/easy
+mkdir -p /home/kali/Learning/thm/linux/medium
+mkdir -p /home/kali/Learning/thm/linux/hard
+mkdir -p /home/kali/Learning/thm/linux/insane
+
+mkdir -p /home/kali/Learning/htb/windows/easy
+mkdir -p /home/kali/Learning/htb/windows/medium
+mkdir -p /home/kali/Learning/htb/windows/hard
+mkdir -p /home/kali/Learning/htb/windows/insane
+mkdir -p /home/kali/Learning/htb/linux/easy
+mkdir -p /home/kali/Learning/htb/linux/medium
+mkdir -p /home/kali/Learning/htb/linux/hard
+mkdir -p /home/kali/Learning/htb/linux/insane
+
+mkdir -p /home/kali/Learning/pwnedlabs/easy
+mkdir -p /home/kali/Learning/pwnedlabs/intermediate
