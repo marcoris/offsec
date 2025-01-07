29dc0ead061e3bdbbba1ef6b7b88535f408b56c8
Marco Ris <ris@contria.com>
2025-01-07 10:50:33 +0100

    Adjust dynamically creating of folders

--- create_folders.sh
+++ create_folders.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 # Creating scripts folder
-mkdir -p /home/kali/Pentesting/scripts/bash
-mkdir -p /home/kali/Pentesting/scripts/php
+mkdir -p "$HOME/Pentesting/scripts/linux/bash"
+mkdir -p "$HOME/Pentesting/scripts/linux/python"
+mkdir -p "$HOME/Pentesting/scripts/windows"
+mkdir -p "$HOME/Pentesting/scripts/php"
 
 # Creating learning folder
-mkdir -p /home/kali/Learning/thm/windows/easy
-mkdir -p /home/kali/Learning/thm/windows/medium
-mkdir -p /home/kali/Learning/thm/windows/hard
-mkdir -p /home/kali/Learning/thm/windows/insane
-mkdir -p /home/kali/Learning/thm/linux/easy
-mkdir -p /home/kali/Learning/thm/linux/medium
-mkdir -p /home/kali/Learning/thm/linux/hard
-mkdir -p /home/kali/Learning/thm/linux/insane
+mkdir -p "$HOME/Learning/thm/windows/easy"
+mkdir -p "$HOME/Learning/thm/windows/medium"
+mkdir -p "$HOME/Learning/thm/windows/hard"
+mkdir -p "$HOME/Learning/thm/windows/insane"
+mkdir -p "$HOME/Learning/thm/linux/easy"
+mkdir -p "$HOME/Learning/thm/linux/medium"
+mkdir -p "$HOME/Learning/thm/linux/hard"
+mkdir -p "$HOME/Learning/thm/linux/insane"
 
-mkdir -p /home/kali/Learning/htb/windows/easy
-mkdir -p /home/kali/Learning/htb/windows/medium
-mkdir -p /home/kali/Learning/htb/windows/hard
-mkdir -p /home/kali/Learning/htb/windows/insane
-mkdir -p /home/kali/Learning/htb/linux/easy
-mkdir -p /home/kali/Learning/htb/linux/medium
-mkdir -p /home/kali/Learning/htb/linux/hard
-mkdir -p /home/kali/Learning/htb/linux/insane
+mkdir -p "$HOME/Learning/htb/windows/easy"
+mkdir -p "$HOME/Learning/htb/windows/medium"
+mkdir -p "$HOME/Learning/htb/windows/hard"
+mkdir -p "$HOME/Learning/htb/windows/insane"
+mkdir -p "$HOME/Learning/htb/linux/easy"
+mkdir -p "$HOME/Learning/htb/linux/medium"
+mkdir -p "$HOME/Learning/htb/linux/hard"
+mkdir -p "$HOME/Learning/htb/linux/insane"
 
-mkdir -p /home/kali/Learning/pwnedlabs/easy
-mkdir -p /home/kali/Learning/pwnedlabs/intermediate
+mkdir -p "$HOME/Learning/pwnedlabs/easy"
+mkdir -p "$HOME/Learning/pwnedlabs/intermediate"
